2b394b4805e348d979262104c702dd9bb75e0415
tariqiqbalofficial <157413892+tariqiqbalofficial@users.noreply.github.com>
2024-02-26 21:14:34 +0500

    Update finalproject21.py

--- finalproject21.py
+++ finalproject21.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import random
 import string
-
+st.title("BANO QABIL 2.0")
 def generate_password(length, use_uppercase, use_numbers, use_special):
     characters = string.ascii_lowercase
     if use_uppercase:
