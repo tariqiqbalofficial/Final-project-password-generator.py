eb532cce762bbaacb123b5460f14059573ebc52f
tariqiqbalofficial <157413892+tariqiqbalofficial@users.noreply.github.com>
2024-02-26 20:35:19 +0500

    Create finaal.py

---
+++ finaal.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import random
+import string
+
+def generate_password(length, use_uppercase, use_numbers, use_special):
+    characters = string.ascii_lowercase
+    if use_uppercase:
+        characters += string.ascii_uppercase
+    if use_numbers:
+        characters += string.digits
+    if use_special:
+        characters += string.punctuation
+
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+def main():
+    st.title("Password Generator")
+
+    length = st.slider("Select password length", 6, 30, 12)
+
+    use_uppercase = st.checkbox("Include Uppercase Letters")
+    use_numbers = st.checkbox("Include Numbers")
+    use_special = st.checkbox("Include Special Characters")
+
+    if st.button("Generate Password"):
+        password = generate_password(length, use_uppercase, use_numbers, use_special)
+        st.success("Your generated password is: ")
+        st.write(password)
+
+if __name__ == "__main__":
+    main()
