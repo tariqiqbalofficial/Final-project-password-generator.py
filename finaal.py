b2c6bdd54f222e22a77eacc4217e64dbf82e1472
tariqiqbalofficial <157413892+tariqiqbalofficial@users.noreply.github.com>
2024-02-28 00:37:30 +0500

    Update finaal.py

--- finaal.py
+++ finaal.py
@@ -15,18 +15,28 @@ def generate_password(length, use_uppercase, use_numbers, use_special):
     return password
 
 def main():
-    st.title("Password Generator")
+    st.image("https://banoqabil.pk", use_column_width=True)
 
-    length = st.slider("Select password length", 6, 30, 12)
+    st.sidebar.title("Navigation")
+    tab = st.sidebar.radio("", ["Home", "About us", "Contact us"])
 
-    use_uppercase = st.checkbox("Include Uppercase Letters")
-    use_numbers = st.checkbox("Include Numbers")
-    use_special = st.checkbox("Include Special Characters")
+    if tab == "Home":
+        st.title("Password Generator")
+        length = st.slider("Select password length", 6, 30, 12)
+        use_uppercase = st.checkbox("Include Uppercase Letters")
+        use_numbers = st.checkbox("Include Numbers")
+        use_special = st.checkbox("Include Special Characters")
 
-    if st.button("Generate Password"):
-        password = generate_password(length, use_uppercase, use_numbers, use_special)
-        st.success("Your generated password is: ")
-        st.write(password)
+        if st.button("Generate Password"):
+            password = generate_password(length, use_uppercase, use_numbers, use_special)
+            st.success("Your generated password is:")
+            st.write(password)
+    elif tab == "About us":
+        st.title("About Us")
+        st.markdown("Team Name: Pythonic Innovators\n\nTeam Members:\n\nTeam Leader: Tariq Iqbal\nMember: Abdul Raffey\nMember: Abdullah Khan\n\nProject Description:\nThe final project submit in Bano Qabil 2.0\nBy Python coding robust password generator developed using Python. The password generator aims to create strong and secure passwords that can be used across various platforms and services. This project is designed to enhance password security and encourage users to use complex, unique passwords for their accounts, thereby improving overall cybersecurity. The generator employs advanced algorithms to ensure the generated passwords are highly secure and difficult to crack, making it an essential tool for anyone concerned about online security.")
+    elif tab == "Contact us":
+        st.title("Contact Us")
+        st.write("Email: www.tariq0213@gmail.com")
 
 if __name__ == "__main__":
     main()
